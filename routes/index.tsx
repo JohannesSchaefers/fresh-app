fa0c199a0518d4d1608c1b25c10b75edde03e16a
JohannesSchaefers <info@iconicity.de>
2025-07-26 16:31:59 +0200

    !!

--- routes/index.tsx
+++ routes/index.tsx
@@ -34,7 +34,7 @@ import DataIsland from "../islands/DataIsland.tsx";
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to the Fresh SSE Example!</h1>
+      <h1>Welcome to the Fresh SSE Example!!</h1>
       <DataIsland />
     </div>
   );
