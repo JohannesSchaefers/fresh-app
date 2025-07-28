4503c6153ff39baf419222000c0667cd33abd42c
JohannesSchaefers <info@iconicity.de>
2025-07-28 16:28:05 +0200

    S S E

--- islands/DataIsland.tsx
+++ islands/DataIsland.tsx
@@ -30,9 +30,9 @@ export default function DataIsland() {
 
   return (
     <div>
-      <h2>Live Data from Server (SSE)</h2>
+      <h2>Live Data from Server ( S S E )</h2>
       <pre
-        style={{ whiteSpace: "pre-wrap", background: "#eee", padding: "1em", borderRadius: "8px" }}
+        style={{ whiteSpace: "pre-wrap", background: "#eee", padding: "1em", borderRadius: "18px" }}
       >
         {data ? JSON.stringify(data, null, 2) : "Waiting for data..."}
       </pre>
